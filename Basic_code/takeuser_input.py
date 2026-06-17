17077a1c2ee22f7b779a6d26b325a47a17ae8707
Rrohan Maurya <rohanmaurya1998@gmail.com>
2026-06-17 19:28:55 +0530

    input program

---
+++ Basic_code/takeuser_input.py
@@ -0,0 +1,12 @@
+#wap to take the user input 
+n1=int(input('enter the number ')) # int data input 
+print(n1)
+n2=eval(input("enter the number ")) # can input int , float , string too
+print(n2)
+n3=input('enter the number ')# any input considered as string
+print(n3)
+
+print(type(n1))
+print(type(n2))
+
+print(type(n3))
