a59cd5552c10f21a18583e0517bb255168ba0ea3
Rrohan Maurya <rohanmaurya1998@gmail.com>
2026-06-17 20:47:34 +0530

    done

---
+++ Basic_code/swap2num_without_temp.py
@@ -0,0 +1,7 @@
+# wap to swap 2 numbers without using the third vzariable 
+a=int(input('enter the number 1  '))#a=6
+b=int(input('enter the number 2  '))#b=5
+a=a+b # a=11
+b=a-b # b=6
+a=a-b #a=5
+print(a,b)
