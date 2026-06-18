938dd82bd0fa509562f2adc9b4a6aea5531e910b
Rrohan Maurya <rohanmaurya1998@gmail.com>
2026-06-18 10:47:45 +0530

    product of even digits

---
+++ number and mathmatical questions/product_of_even_digits.py
@@ -0,0 +1,10 @@
+#wap to print the product of even digits
+n=int(input('enter the number '))
+product=1
+while n>0:
+    for i in range(len(str(n))):
+        digit=n%10
+        if digit%2==0:
+            product*=digit
+        n=n//10
+print(product)
---
+++ number and mathmatical questions/sum_of_even_digits.py
@@ -0,0 +1,10 @@
+# wap to print the sum of even digits
+n=int(input('enter the number '))
+sum=0
+while n>0:
+    for i in range(len(str(n))):
+        digit=n%10
+        if digit%2==0:
+            sum+=digit
+        n=n//10
+print(sum)
