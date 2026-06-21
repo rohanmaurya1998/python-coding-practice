a8b1901f8c9708edc465443c26b15fefd44dc098
Rrohan Maurya <rohanmaurya1998@gmail.com>
2026-06-21 17:25:43 +0530

    hcf of n numbers

--- number and mathmatical questions/hcf_of_n_numbers.py
+++ number and mathmatical questions/hcf_of_n_numbers.py
@@ -1,5 +1,13 @@
 #wap to print the hcf of n numbers
-nums=[12,15,18,20,27]
-smaller=min(12,15,18,20,27)
+nums=list(map(int,input('enter numbers\n').split()))
+smaller=min(nums)
+hcf=1
 for i in range(1,smaller+1):
-    if
+    common =True
+    for num in nums:
+        if num%i !=0:
+            common=False
+            break
+    if common:
+        hcf=i
+print(hcf)
